84c81b726e046b99fda48b456841860a9fa7c611
takahasiyuuma <73160655+takahasiyuuma@users.noreply.github.com>
2021-04-08 07:02:56 +0900

    Add files via upload

---
+++ WisdomWorld/broom_rotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class broom_rotation : MonoBehaviour
+{
+
+    [SerializeField] private Transform handTrans_L = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //コントローラー時↓
+        //transform.rotation = handTrans_L.rotation * Quaternion.Euler(35, -180, 0);
+
+        //ヘットセット時↓
+        transform.rotation = handTrans_L.rotation * Quaternion.Euler(0, -180, 0);
+    }
+}
